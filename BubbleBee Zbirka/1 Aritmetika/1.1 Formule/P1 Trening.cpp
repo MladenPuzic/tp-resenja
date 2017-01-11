d092dc04c405b9076244d9605245687f3dfca3e4
theSitzr <mladjoni12@gmail.com>
2017-01-11 19:09:03 +0100

    Create P1 Trening.cpp

---
+++ BubbleBee Zbirka/1 Aritmetika/1.1 Formule/P1 Trening.cpp
@@ -0,0 +1,13 @@
+//Problem: 1.1 - trening
+//Autor rešenja: Mladen Puzić
+//Put koji sportista pređe jednak je obimu pravougaonika koji se računa formulom O = 2*(d+s)
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	int d, s;
+	scanf("%d%d", &d, &s);
+	printf("%d", 2*(d+s));
+	return 0;
+}
