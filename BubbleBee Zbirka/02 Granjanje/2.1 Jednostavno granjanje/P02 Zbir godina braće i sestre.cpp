f5f392e8db2f52dcab4fdaea4fd271569149cc56
theSitzr <mladjoni12@gmail.com>
2017-01-14 21:36:15 +0100

    Update P02 Zbir godina braće i sestre.cpp

--- BubbleBee Zbirka/02 Granjanje/2.1 Jednostavno granjanje/P02 Zbir godina braće i sestre.cpp
+++ BubbleBee Zbirka/02 Granjanje/2.1 Jednostavno granjanje/P02 Zbir godina braće i sestre.cpp
@@ -1,5 +1,7 @@
 //Problem: 2.1 - Zbir godina braće i sestre
 //Autor rešenja: Mladen Puzić
+//Ako je broj godina jednog brata x, onda je ukupan zbir 4*x + 3, onda je x = (p-3)/4
+//Ako je to ceo nenegativan broj, onda je odgovor da
 #include <cstdio>
 using namespace std;
 
