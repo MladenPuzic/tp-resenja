bcd289ed8cac256557cd4afaeb8f1d18885427dc
theSitzr <mladjoni12@gmail.com>
2017-01-14 22:01:54 +0100

    Create P01 Kuglice.cpp

---
+++ BubbleBee Zbirka/02 Granjanje/2.2 Minimum i maksimum/P01 Kuglice.cpp
@@ -0,0 +1,13 @@
+//Problem: 2.2 - Kuglice
+//Autor rešenja: Mladen Puzić
+//Iz jedne moramo izbaciti sve crvene, a iz druge sve plave, pa je rezultat manji od c1+p2 i p1+c2
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int c1, p1, c2, p2;
+	scanf("%d%d%d%d", &c1, &p1, &c2, &p2);
+  printf("%d", min(c1 + p2, c2 + p1));
+	return 0;
+}
