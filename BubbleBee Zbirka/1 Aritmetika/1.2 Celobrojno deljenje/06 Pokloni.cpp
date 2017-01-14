7e9f64b49fa83c8a8b007bc295090bc438eda4af
theSitzr <mladjoni12@gmail.com>
2017-01-14 18:25:38 +0100

    Create 06 Pokloni.cpp

---
+++ BubbleBee Zbirka/1 Aritmetika/1.2 Celobrojno deljenje/06 Pokloni.cpp
@@ -0,0 +1,14 @@
+//Problem: 1.2 - Pokloni
+//Autor rešenja: Mladen Puzić
+
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+	int n, k;
+	scanf("%d%d", &n, &k);
+	int res = n/k;
+	printf("%d", res);
+	return 0;
+}
