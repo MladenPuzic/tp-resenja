006425a38288acbb6f63de7c5eb35d05e8aaca0b
theSitzr <mladjoni12@gmail.com>
2017-01-14 21:30:59 +0100

    Create P01 Jabuke.cpp

---
+++ BubbleBee Zbirka/02 Granjanje/2.1 Jednostavno granjanje/P01 Jabuke.cpp
@@ -0,0 +1,12 @@
+//Problem: 2.1 - Jabuke
+//Autor rešenja: Mladen Puzić
+#include <cstdio>
+using namespace std;
+
+int main(){
+	int p, m;
+	scanf("%d%d", &p, &m);
+	if(p > m) printf("DA");
+	else printf("NE");
+	return 0;
+}
